1f4c9ab2ed0029696e1834cf714a26c7541067fc
Aleksandr Demidov <demidoff@1vp.ru>
2022-04-22 14:14:46 +0500

    The first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from Products.CustomZMI import __version__
+
+setup(
+    name='Products.CustomZMI',
+    version=__version__,
+
+    url='https://github.com/sashad/Products.CustomZMI.git/',
+    author='Aleksandr Demidov',
+    author_email='demidoff@1vp.ru',
+
+    py_modules=['Products.CustomZMI'],
+)
