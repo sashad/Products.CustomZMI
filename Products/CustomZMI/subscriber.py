4536df41aab39927df20ea103eb4d02f7d33c11d
Aleksandr Demidov <demidoff@1vp.ru>
2022-04-23 18:21:47 +0500

    The first commit

--- Products/CustomZMI/subscriber.py
+++ Products/CustomZMI/subscriber.py
@@ -6,10 +6,14 @@ from zmi.styles import subscriber
 @zope.component.adapter(zope.interface.Interface)
 def css_paths(context):
     """Return paths to CSS files needed for the Zope 4 ZMI."""
-    return ('/zmi.css',)
+    if hasattr(context, 'zmi.css'):
+        return ('/zmi.css',)
+    return None
 
 
 @zope.component.adapter(zope.interface.Interface)
 def js_paths(context):
     """Return paths to JS files needed for the Zope 4 ZMI."""
-    return ('/zmi.js',)
+    if hasattr(context, 'zmi.js'):
+        return ('/zmi.js',)
+    return None
