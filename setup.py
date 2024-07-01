4f119e461dd6999b3baaf5ca61865c5ca8323836
Aleksandr Demidov <demidoff@1vp.ru>
2024-07-01 14:25:07 +0500

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='demidoff@1vp.ru',
 
     packages=['Products.CustomZMI'],
-    package_data={'': ['configure.zcml']},
+    package_data={'': ['configure.zcml', 'resources']},
     include_package_data=True,
     install_requires=[
         'Zope>=5.5.0',
